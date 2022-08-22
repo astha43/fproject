f7e0ddcfc7415ca24fa74c68f34affe2e7d9cad9
astha singh <astha@triazinesoft.com>
2022-08-22 11:28:31 +0530

    first_19_08

--- app/src/main/java/com/example/recyclerviewapplication/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewapplication/MainActivity.kt
@@ -58,7 +58,7 @@ class MainActivity : AppCompatActivity() {
 
         newArrayList = arrayListOf<DataModel>()
 
-        getUserData()
+           getUserData()
     }
 
     private fun getUserData() {
